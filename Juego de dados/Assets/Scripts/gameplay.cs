57afb281f9fbcc60abf627f9d20e7cc62d2236c8
StefanoCvitanich <stefanojc@gmail.com>
2017-04-24 22:26:12 -0300

    Unity Ads
    No funciona :(

--- Juego de dados/Assets/Scripts/gameplay.cs
+++ Juego de dados/Assets/Scripts/gameplay.cs
@@ -2,44 +2,77 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Advertisements;
 
-public class gameplay : MonoBehaviour {
+public class gameplay : MonoBehaviour
+{
 
-	public Text dadosPlayer;
-	public Text dadosOponente;
-	private int randomPlayer = 0;
-	private int randomOpponent = 0;
-	private string value;
-	private int wins = 0;
-	public Text victorias;
+    public Text dadosPlayer;
+    public Text dadosOponente;
+    private int randomPlayer = 0;
+    private int randomOpponent = 0;
+    private string value;
+    private int wins = 0;
+    public Text victorias;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	public void rollDice() {
-	
-		randomPlayer = Random.Range (2,12);
+    }
 
-		value = randomPlayer.ToString ();
+    // Update is called once per frame
+    void Update()
+    {
 
-		dadosPlayer.text = value;
+    }
 
-		randomOpponent = Random.Range (2, 12);
+    public void rollDice()
+    {
 
-		value = randomOpponent.ToString ();
+        randomPlayer = Random.Range(2, 12);
 
-		dadosOponente.text = value;
+        value = randomPlayer.ToString();
 
-		if (randomPlayer > randomOpponent)
-			wins++;
+        dadosPlayer.text = value;
 
-		victorias.text = "Victorias: " + wins.ToString ();
-	}
+        randomOpponent = Random.Range(2, 12);
+
+        value = randomOpponent.ToString();
+
+        dadosOponente.text = value;
+
+        if (randomPlayer > randomOpponent)
+            wins++;
+
+        victorias.text = "Victorias: " + wins.ToString();
+
+        if (wins >= 1)
+        {
+            ShowAd();
+        }
+    }
+
+    private void ShowAd()
+    {
+        if (Advertisement.IsReady("rewardedVideo"))
+        {
+
+            ShowOptions debuggeo = new ShowOptions { resultCallback = laquequieras };
+            Advertisement.Show("rewardedVideo", debuggeo);
+        }
+    }
+
+    private void laquequieras(ShowResult result) {
+
+        switch (result) {
+            case ShowResult.Finished:
+                Debug.Log("Se mostro el auncio");
+                break;
+            case ShowResult.Failed:
+                Debug.Log("Fallo el anuncio");
+                break;
+        }
+
+    }
 }
